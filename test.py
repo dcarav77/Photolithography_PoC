5623cf08876fe00a9a573559d349d9bdcbac6146
dcarav77 <dcarav77@gmail.com>
2025-01-15 12:44:56 -0500

    deleted test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import random
-import matplotlib.pyplot as plt
-import networkx as nx
-from owlready2 import *
-import tkinter as tk
-from tkinter import ttk, messagebox, filedialog
-import pandas as pd
-
-print("All packages installed and imported successfully!")
