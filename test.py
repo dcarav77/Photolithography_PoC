04895e7578e09b008f841c4950d1336b1fc8e9fe
dcarav77 <dcarav77@gmail.com>
2025-01-15 11:51:09 -0500

    initial commit

---
+++ test.py
@@ -0,0 +1,9 @@
+import random
+import matplotlib.pyplot as plt
+import networkx as nx
+from owlready2 import *
+import tkinter as tk
+from tkinter import ttk, messagebox, filedialog
+import pandas as pd
+
+print("All packages installed and imported successfully!")
